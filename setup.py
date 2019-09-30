d5687389cf2fed7058a21200362cfe897a104594
Neha Keshan <neha1930@gmail.com>
2019-09-30 16:13:19 -0400

    converting metamine KG as a whyis application

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='metamine-visualization',
+      version='0.1',
+      description='visualization for metamine knowledge graph',
+      author='Neha Keshan',
+      packages=[
+          
+          ],
+     )
